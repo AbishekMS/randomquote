20d54880a1c89b8646aedd29af8eaac1cf8ca38c
AbishekMS <108707094+AbishekMS@users.noreply.github.com>
2023-04-21 15:13:48 +0530

    Create footer.js

---
+++ src/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer(){
+    return(
+        <div style={{background:"grey",textAlign:"center",position:'static' ,paddingTop:10,paddingBottom:10,fontSize:30}}>
+        <p>by  <b> Abishek M S </b></p>
+        </div>
+    )
+}
+export default Footer;
