a72f07f864fda03a0fb1fdf1690cd96a651b9815
AbishekMS <108707094+AbishekMS@users.noreply.github.com>
2023-04-21 15:13:04 +0530

    Create header.js

---
+++ src/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Header(){
+    return(
+        <div style={{background:"grey",textAlign:"center" ,paddingTop:10,paddingBottom:10,fontSize:30}}>
+        <b>Random Quotes Machine </b>
+        </div>
+    )
+}
+export default Header;
