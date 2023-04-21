31a66926de9e8c625fa9f35afaf12b986d598afe
AbishekMS <108707094+AbishekMS@users.noreply.github.com>
2023-04-21 15:12:00 +0530

    Update project.js

--- src/project.js
+++ src/project.js
@@ -4,6 +4,8 @@ import './pro.css';
 import { FaTwitter } from 'react-icons/fa';
 import {BsQuote} from 'react-icons/bs';
 import {AiOutlineLine} from 'react-icons/ai';
+import Header from './header';
+import Footer from './footer';
 
 function QuoteBox({quote,handleQuote}){
  
@@ -16,34 +18,31 @@ function QuoteBox({quote,handleQuote}){
 
   return(
     <div>
-    <header  style={{textAlign:"center", paddingBottom:80, paddingTop:2 ,color:"grey"}}>
-    <h1><u>Random Quote Machine</u></h1>
-    </header>
+    <Header/>
 
-    <div id='quote-box'  style={{backgroundColor:"white"}}>
+    <div id='quote-box'  style={{marginBottom:123,marginTop:123,position:'sticky'}}>
     <div id="innerBox">
 
     <p style={{paddingLeft:120, color:"orange"}}><BsQuote/></p>
+  
     <p id='text' ref={textRef}> {quote.text}<br/>
     <p style={{color:"orange",paddingLeft:110}}>
     <AiOutlineLine  /><AiOutlineLine/></p>
-    <h3 id='author' style={{textAlign:"center",paddingBottom:25}}>{quote.author}</h3>
+    <h3 id='author'   style={{textAlign:"center",paddingBottom:25}}>{quote.author}</h3>
      </p>
- 
+     
     <div>
     <Button id='new-quote' variant="contained" onClick={handleQuote} sx={{marginBottom:3}}>New-Quote</Button>
   
     <a target="_blank" rel="noopener noreferrer" 
-    href= {"https://twitter.com/intent/tweet?hashtags=RandomQuotesProject&hashtags=quotes&hashtags=abishek&text=" +
+    href= {"https://twitter.com/intent/tweet?hashtags=RandomQuotesProject&hashtags=quotes&text=" +
      encodeURI(quote.text + " -- " + quote.author)
     }
     id='tweet-quote' ><FaTwitter style={{fontSize:30}}/></a>
     </div>
     </div>
     </div>
-    <footer  style={{color:"red", textAlign:"center",paddingTop:120, paddingBottom:20}}>
-    <p><u>by Abishek.M.S</u></p>
-  </footer>
+    <Footer/>
   </div>
   )
 }
@@ -55,6 +54,7 @@ function Rand(){
  
  const getQuotes=()=>{
    fetch("https://type.fit/api/quotes")
+
    .then((res)=>res.json())
    .then((data)=>{
     let randomNumber =Math.round(Math.random()*(data.length-1))
